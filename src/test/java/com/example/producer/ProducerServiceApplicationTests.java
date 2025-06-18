2370ebe16fc89a269e85a9262b846e7813417629
Jacob A <jacobm00517@hotmail.com>
2025-06-18 18:05:07 -0400

    Initialized test class.

---
+++ src/test/java/com/example/producer/ProducerServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.producer.ProducerServiceApplication;
+
+@SpringBootTest(classes = ProducerServiceApplication.class)
+public class ProducerServiceApplicationTests {
+}
